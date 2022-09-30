6c3bdb26393d33420885632b40b0ffab6fa5f342
Mosquito638 <113045592+Mosquito638@users.noreply.github.com>
2022-09-30 20:37:33 +0300

    Create DZ_4.cs
    Домашняя работа_4

---
+++ DZ_4.cs
@@ -0,0 +1,73 @@
+// Задача 25: напишите цикл, который принимает на вход 2
+// числа (А и В) и возводит число А в натуральную степень В
+// 3,5 -> 243 3(5)
+// 2,4 -> 16
+
+// Console.Write("Введите первое число A: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число B: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// int i = 1;  
+// int Pr = A;
+
+// while (i<B)
+// {
+//     Pr = Pr*A;
+//     i = i+1;
+// }
+
+// Console.Write((A) + "," + (B) + " - > " + (Pr));
+
+
+// Задача 27: Напишите программу, которая принимает на вход
+//  число и выдает сумму цифр в числе
+// 452 -> 11
+// 82-> 10
+// 9012 -> 12
+
+// Console.Write("Введите число: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+
+// int count = A;
+// int Sum = 0;
+
+// while (count > 0)
+// {
+//     Sum = Sum + count%10;
+//     count = count / 10;
+// }
+// Console.Write((A) + " -> " + (Sum));
+
+
+//  Задача 29: Напишите программу, которая задает массив из 8
+//  элементов и выводит их на экран
+// 1,2,5,7,19 -> [1,2,5,7,19]
+// 6,1,33 -> [6,1,33]
+
+
+// ____________________________________
+// int[] array = new int[8];
+// Random RN = new Random();
+// int i=0;
+// int l = array.Length;
+//     int count = array.Length;
+//     while (i < l)
+//     {
+//         if (i < l-1)
+//         {
+//             array[i] = RN.Next(0,10);
+//             Console.Write(array[i] + ",");
+//             i = i+1;
+//         }
+//         else
+//         {
+//             Console.Write(array[i]);
+//             i = i+1;
+//         } 
+//     }
+
+//     Console.WriteLine( " -> " + "[" + (array[0]) + "," + (array[1]) + "," + (array[2]) + "," + (array[3]) + "," + (array[4]) + "," + (array[5]) + "," + (array[6]) + "," + (array[7]) + "]" );
+// _________________________________________________________________________________
