de6edc6dfca04f2c93d0b30f12e921c1fd4cf50a
Mosquito638 <113045592+Mosquito638@users.noreply.github.com>
2022-10-18 11:57:12 +0300

    Create Recursiya.cs
    решено 2 задачи из 3

---
+++ Recursiya.cs
@@ -0,0 +1,36 @@
+// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+// Console.WriteLine($"Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write($" N = {num} -> ");
+// int NatNums(int num)
+// {
+//     if(num == 0) return num;
+//     Console.Write($"{num},");return num = NatNums(num-1);   
+// }
+// int a = NatNums(7);
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт
+//  сумму натуральных элементов в промежутке от M до N.
+
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+// 
+
+// Console.WriteLine($"Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+
+// int NatCount(int n, int m)
+// {
+//     if (n > m-1 ) return n;
+//     return n = n + NatCount(n+1, m);
+// }
+// Console.Write($"{NatCount(n,m)}");
